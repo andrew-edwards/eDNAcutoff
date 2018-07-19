74595e7cc4829c763e1685b2887ef3b2282e4092
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-07-19 16:57:20 -0700

    Add example spot.

--- R/remove_false_pos.R
+++ R/remove_false_pos.R
@@ -21,6 +21,8 @@
 ##' @return A tbl_df with susepcted false positive reads set to 0.
 ##' @author Andrew Edwards
 ##' @export
+##' @examples
+##'
 remove_false_pos = function(data, mT=4, category = FALSE, tol = 0.2, ...) {
   if(class(data)[1] != "tbl_df") stop("First argument needs to be a dataframe.")
   if(!("mock" %in% data$Sample_name)) stop("Need a mock sample.")
