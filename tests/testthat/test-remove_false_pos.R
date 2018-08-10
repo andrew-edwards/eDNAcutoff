c90bc067e5af66595a2f556a8f71614fc4dae6ca
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-08-10 15:26:05 -0700

    Unit test the big example also.

--- tests/testthat/test-remove_false_pos.R
+++ tests/testthat/test-remove_false_pos.R
@@ -3,3 +3,8 @@ context("test-remove_false_pos.R")
 test_that("false positives are correctly removed in small example", {
     expect_equal(remove_false_pos(small_example), small_example_result)
 })
+
+test_that("false positives are correctly removed in big example", {
+    expect_equal(remove_false_pos(big_example), big_example_result)
+})
+
