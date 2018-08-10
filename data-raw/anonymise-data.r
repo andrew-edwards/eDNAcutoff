e5d370145b7406aff21d953f6cfb533198ccf4eb
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-08-10 13:30:00 -0700

    Add small example expected result.

--- data-raw/anonymise-data.r
+++ data-raw/anonymise-data.r
@@ -8,19 +8,14 @@ big_example <- readr::read_csv("data-raw/data_for_andy.csv",
 big_example <- dplyr::filter(big_example, Category_Sample_blank != "b") %>%
     dplyr::select(-Category_Sample_blank)
 
-# Anonymise
+# Anonymise and save
 names(big_example) <- c("Sample", "m1", "m2", "m3", "m4",
                         1:(length(names(big_example))-5))
 big_example$Sample = c("M", 1:(length(big_example$Sample)-1))
 
 usethis::use_data(big_example, overwrite = TRUE)
 
-
-# From Sean's course:
-survey_dat <- readr::read_csv("data-raw/petrale-qcs-survey.csv")
-# survey_dat <- readr::read_csv("petrale-qcs-survey.csv")
-usethis::use_data(survey_dat, overwrite = TRUE)
-
+# Do same for small example
 
 small_example <- readr::read_csv("data-raw/data_manual_test.csv",
                                comment="#")
@@ -28,9 +23,17 @@ small_example <- readr::read_csv("data-raw/data_manual_test.csv",
 # Remove blank samples and the Category_Sample_blank column
 small_example <- dplyr::select(small_example, -Category_Sample_blank)
 
-# Anonymise
+# Anonymise and example
 names(small_example) <- c("Sample", "m1", "m2", "m3", "m4",
                         1:(length(names(small_example))-5))
-small_example$Sample = c("M", 1:(length(small_example$Sample)-1))
+small_example$Sample <- c("M", 1:(length(small_example$Sample)-1))
 
 usethis::use_data(small_example, overwrite = TRUE)
+
+# Save here to then manually edit for expected result after using
+#  remove_false_pos().  So if re-run this line then have to manually edit again.
+# readr::write_csv(small_example, "data-raw/data_manual_test_result.csv")
+
+small_example_result <- readr::read_csv("data-raw/data_manual_test_result.csv")
+
+usethis::use_data(small_example_result, overwrite = TRUE)
