d960a2e7b4c7e812a16f745d8ac8f5ccf2008ded
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2018-08-24 17:12:15 -0700

    Update change_alpha().

--- R/change_alpha.R
+++ R/change_alpha.R
@@ -3,7 +3,7 @@
 ##' @description Removes false positives for various values of the tolerance parameter alpha
 ##' @param data As for remove_false_pos()
 ##' @param alpha.vec Vector of alpha (tolerance) values over which to run remove_false_pos().
-##' @param mN Total number of mock species.
+##' @param aT Total number of mock species.
 ##' @return List consisting of:
 ##'    ***:  list of dataframes, one for each result of the corresponding
 ##'    value of alpha;
@@ -11,7 +11,7 @@
 ##'    num.mock.sp.left: ***
 ##'    num.samp.with.mock: ***
 ##' @author Andrew Edwards
-change_alpha = function(data, alpha.vec = seq(0, 1, 0.1), mN = 4) {
+change_alpha = function(data, alpha.vec = seq(0, 1, 0.1), aT = 4) {
 
   N <- length(alpha.vec)
   out.list = list()       # Will be a list of dataframes, each being the result for
@@ -22,12 +22,12 @@ change_alpha = function(data, alpha.vec = seq(0, 1, 0.1), mN = 4) {
 
   for(i in 1:N)
     {
-      out.list[[i]] = remove_false_pos(data, mN = mN, alpha = alpha.vec[i])
+      out.list[[i]] = remove_false_pos(data, aT = aT, alpha = alpha.vec[i])
       diff = which_set_to_zero(data, out.list[[i]])
 
       num.zeroed[i] = sum(as.vector(diff) > 0,
                            na.rm = TRUE)    # CHECK doesn't give warnings
-      mock.sp.out = out.list[[i]][2:dim(out.list[[i]])[1], 2:(mN + 1)]
+      mock.sp.out = out.list[[i]][2:dim(out.list[[i]])[1], 2:(aT + 1)]
         # just mock species in non-mock samples. **assumes one mock sample
       # num.mock.sp.zeroed[i] = sum(as.vector(mock.sp.rem) > 0)
       num.mock.sp.left[i] = sum(as.vector(mock.sp.out > 0))
